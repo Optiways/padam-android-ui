a32a52601932045f6addf0a1049f64599e54c69f
Kiefer Wiessler <kiefer@padam.io>
2024-05-30 15:20:13 +0200

    [Style] - PUISpace

---
+++ src/main/java/io/padam/android_ui/customer/styles/PUISpace.kt
@@ -0,0 +1,15 @@
+package io.padam.android_ui.customer.styles
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+val PUISpace1: Dp = 4.dp
+val PUISpace2: Dp = 8.dp
+val PUISpace3: Dp = 12.dp
+val PUISpace4: Dp = 16.dp
+val PUISpace5: Dp = 24.dp
+val PUISpace6: Dp = 32.dp
+val PUISpace7: Dp = 40.dp
+val PUISpace8: Dp = 48.dp
+val PUISpace9: Dp = 64.dp
+val PUISpace10: Dp = 80.dp
